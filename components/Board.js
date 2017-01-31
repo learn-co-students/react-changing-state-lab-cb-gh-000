3f576733da8edfbe35ed38865bc49d663beb5980
jaabiri salim <jaabiri_salim@hotmail.com>
2017-01-31 18:30:06 +0000

    Done.

--- components/Board.js
+++ components/Board.js
@@ -3,12 +3,17 @@ const Field = require('./Field');
 
 class Board extends React.Component {
   render () {
-    const { board, onClick } = this.props;
-    return (
-      <div>
-      </div>
-    );
-  }
+   const { board, onClick } = this.props;
+   return (
+     <div className="board">
+       {board.map((player,i)=>{
+           return (
+             <Field player={player} key={i} onClick={/*(e)=>{onClick(i,e)}*/ onClick.bind(null,i)} />
+           )
+       })}
+     </div>
+   );
+ }
 }
 
 module.exports = Board;
--- components/Game.js
+++ components/Game.js
@@ -3,33 +3,69 @@ const Board = require('./Board');
 const Status = require('./Status');
 const solutions = require('./solutions');
 
+
 class Game extends React.Component {
+
   constructor (props) {
-    super(props);
-    this.state = {};
+   super(props);
+   this.state = {
 
-    this.handleReset = this.handleReset.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+     board:[null,null,null,null,null,null,null,null,null],
+     turn:"X"
+   };
 
-  handleReset (ev) {
-  }
+   this.handleReset = this.handleReset.bind(this);
+   this.handleClick = this.handleClick.bind(this);
+ }
 
-  handleClick (i, ev) {
-  }
+ handleReset (ev) {
+   ev.preventDefault();
+   this.setState({
+      board:[null,null,null,null,null,null,null,null,null],
+      turn:"X"
+   })
+ }
 
-  getWinner () {
-  }
+ handleClick (i, ev) {
+  ev.preventDefault();
 
-  isComplete () {
-  }
 
-  render () {
-    return (
-      <div>
-      </div>
+       let board = this.state.board.slice()
+        board[i]=this.state.turn
+     this.setState({
+        board:board,
+        turn :this.state.turn==="X"?"O":"X"
+     })
+
+
+ }
+
+ getWinner () {
+
+
+ const results = solutions.map(
+      (solution) => solution.map((i) => this.state.board[i]).join('')
     );
-  }
+    const el = results.find(
+      (item) => item === 'XXX' || item === 'OOO'
+    );
+    return el && el[0];
+
+ }
+
+ isComplete () {
+   return  this.state.board.filter(x=>x).length === this.state.board.length
+ }
+
+ render () {
+   return (
+     <div className="game">
+         <Board   board={this.state.board} onClick={this.handleClick} />
+          { this.isComplete() ? <Status winner={this.getWinner()} /> : null }
+         <button className='game__reset'  onClick={this.handleReset}> reset</button>
+     </div>
+   );
+ }
 }
 
 module.exports = Game;
