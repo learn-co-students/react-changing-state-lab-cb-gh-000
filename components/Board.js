27e86851f17b1530277275bba27f97f5b030805f
Sai Rahul <rahulsharma7651@gmail.com>
2017-05-26 11:42:29 +0000

    Done.

--- components/Board.js
+++ components/Board.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import Field from './Field';
+const React = require('react');
+const Field = require('./Field');
 
-export default class Board extends React.Component {
+class Board extends React.Component {
   render () {
-    const { board, onClick } = this.props;
-    return (
-      <div>
-      </div>
-    );
-  }
+   const { board, onClick } = this.props;
+   return (
+     <div className="board">
+       {board.map((player,i)=>{
+           return (
+             <Field player={player} key={i} onClick={/*(e)=>{onClick(i,e)}*/ onClick.bind(null,i)} />
+           )
+       })}
+     </div>
+   );
+ }
 }
+
+module.exports = Board;
